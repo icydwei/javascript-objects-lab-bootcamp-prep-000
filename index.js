621ac6e60df9c91bd320f0324fba1b26851920b1
Rob Paik <robpaik@gmail.com>
2019-06-16 20:06:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+  var newobj = Object.assign({}, object)
+  delete newobj.key
+  return newobj
 }
