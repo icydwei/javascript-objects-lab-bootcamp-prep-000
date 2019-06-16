c116d5f10ebffffd872bb98ee905756fcab93ca7
Rob Paik <robpaik@gmail.com>
2019-06-16 20:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue (object, key, value) {
   return object
 }
 
-function destructivelyUpdateObjectWithKeyandValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object.key = value;
   return object
 }
